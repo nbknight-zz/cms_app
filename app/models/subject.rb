e85baef373cb86afb656ae5dba4dcb6ded7e0dbb
nbknight <nicholas.b.knight@gmail.com>
2018-08-27 22:33:12 -0400

    Add associations to models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,12 @@
 class Subject < ApplicationRecord
+
+	has_many :pages
+
+
+	scope :visible, lambda { where(:visible => true) }
+	scope :invisible, lambda { where(:visible => false) }
+	scope :sorted, lambda { order(:position) }
+	scope :newest_first, lambda { order("created_at DESC") }
+	scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"]) }
+
 end
