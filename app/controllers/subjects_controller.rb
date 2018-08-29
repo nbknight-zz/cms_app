8f48f41c0ea69eaa7e20aff2cb770720fb2c0cc8
nbknight <nicholas.b.knight@gmail.com>
2018-08-28 22:17:41 -0400

    Add Form page. Build create and new methods

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,9 +9,21 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => 'Default'})
   end
 
   def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(params[:subject])
+    # Save the object
+    if @subject.save
+    # If save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+    # If save fails, redisplay the form so user can fix problems
+    render('new')
+    end
+    
   end
 
   def edit
